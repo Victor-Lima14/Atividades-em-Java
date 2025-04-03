e58c6b176f794d5f9f2b16992135a7b0697b2706
VictorLima <victorrafaellimadesantana14@gmail.com>
2025-04-03 16:11:39 -0300

    Delete Banco.java

--- Banco.java
+++
@@ -1,8 +0,0 @@
-public class Banco {
-
-    public static void main(String[] args){
-
-        ContaBancaria conta1 = new ContaBancaria("Victor", 5000, 2000, 0);
-        System.out.println("O nome do Usuario, seu Saldo, Quantidade para Deposito e Quantidade para Saque são: " +"\n" +conta1.getTitular() +"\n" +conta1.getSaldo() +"\n" +conta1.getDepositar() +"\n" +conta1.getSacar());
-    }
-}
